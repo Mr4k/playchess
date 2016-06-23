a11b80e58ca297f783310c3388027f4f5cb8dd6f
Peter Stefek <peterlikesemail@gmail.com>
2016-06-23 16:02:41 -0700

    building backend

--- play.js
+++ play.js
@@ -1,21 +1,54 @@
-var board, game = new Chess();
+var board,
+  game = new Chess();
+
+// do not pick up pieces if the game is over
+// only pick up pieces for White
+var onDragStart = function(source, piece, position, orientation) {
+  if (game.in_checkmate() === true || game.in_draw() === true ||
+    piece.search(/^b/) !== -1) {
+    return false;
+  }
+};
 
 var makeRandomMove = function() {
   var possibleMoves = game.moves();
 
-  // exit if the game is over
-  if (game.game_over() === true ||
-    game.in_draw() === true ||
-    possibleMoves.length === 0) return;
+  // game over
+  if (possibleMoves.length === 0) return;
 
   var randomIndex = Math.floor(Math.random() * possibleMoves.length);
   game.move(possibleMoves[randomIndex]);
   board.position(game.fen());
-
-  window.setTimeout(makeRandomMove, 500);
 };
 
-board = ChessBoard('board', 'start');
-$("#board").append("<div class='loader'>Loading...</div>")
+var onDrop = function(source, target) {
+  // see if the move is legal
+  var move = game.move({
+    from: source,
+    to: target,
+    promotion: 'q' // NOTE: always promote to a queen for example simplicity
+  });
 
-window.setTimeout(makeRandomMove, 500);
+  // illegal move
+  if (move === null) return 'snapback';
+
+  // make random legal move for black
+  window.setTimeout(makeRandomMove, 250);
+};
+
+// update the board position after the piece snap
+// for castling, en passant, pawn promotion
+var onSnapEnd = function() {
+  board.position(game.fen());
+};
+
+var cfg = {
+  draggable: true,
+  position: 'start',
+  onDragStart: onDragStart,
+  onDrop: onDrop,
+  onSnapEnd: onSnapEnd
+};
+
+board = ChessBoard('board', cfg);
+$("#board").append("<div class='loader'>Loading...</div>")
---
+++ server/game_trainer.py
@@ -0,0 +1,10 @@
+import chess
+import tensorflow as tf
+
+def train_model():
+	x = tf.placeholder(tf.float32, [None, 768])
+	y = tf.placeholder(tf.float32)
+
+def build_feature_vector_from_board(board):
+	
+	
