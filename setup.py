df0d7bce9fbd3f93d1a09b86fcc1f7b1bee8e734
Peter Stefek <peterlikesemail@gmail.com>
2016-06-28 13:41:46 -0700

    preparing to deploy to openshift

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='PlayChess',
+      version='0.1',
+      description='A ches engine with a web interface',
+      author='Peter Stefek',
+      author_email='pstefek@oberlin.edu',
+      url='https://playchess-pstefek.rhcloud.com',
+     install_requires=['Flask>=0.10.1'],
+     )
